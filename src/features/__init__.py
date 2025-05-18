b15df153af79f4f72ab49ab9078cb96adeb4edee
Yanxun Zhou <yanxunzhou0927@gmail.com>
2025-05-19 01:18:07 +0800

    Complete main program data analysis functionality

---
+++ src/features/__init__.py
@@ -0,0 +1,17 @@
+import logging
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+if not logger.handlers:
+    handler = logging.StreamHandler()
+    formatter = logging.Formatter('[%(levelname)s] %(name)s: %(message)s')
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+
+logger.info("📦 載入 features 模組")
+
+import features.timeseries_features
+#from .feature_selector import select_top_features
+
+__all__ = ["timeseries_features"]
+__version__ = "0.1.0"
